2776fb4b4d999e90bb1365ef964e0479ec742a75
Vernon <fightstar@126.com>
2015-02-26 14:48:23 +0800

    最新代码

---
+++ app/src/main/java/com/ciwei/client/model/ResultBean.java
@@ -0,0 +1,13 @@
+package com.ciwei.client.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * Created by Vernon on 15/2/26.
+ */
+public class ResultBean implements Serializable {
+
+    private int retCode;
+
+}
